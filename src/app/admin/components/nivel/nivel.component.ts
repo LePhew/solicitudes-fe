40517b50f237a9cb1aedb99cb1746281c68714ba
LePhew <marcos_eduardo16@hotmail.com>
2019-07-21 23:38:32 -0400

    Mantenimiento nivel

--- src/app/admin/components/nivel/nivel.component.ts
+++ src/app/admin/components/nivel/nivel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NivelDTO } from 'src/app/models/Nivel';
+import { GenericService } from 'src/app/shared/services/generic-service';
 
 @Component({
   selector: 'app-nivel',
@@ -7,12 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NivelComponent implements OnInit {
 
-  constructor() { }
+  readonly pagename: string = "Gestión de Niveles";
+  readonly componentUrl: string = "nivel/";
+  
+  niveles: any[];
+  nivel: NivelDTO;
+  editMode: boolean = false;
+  
+  constructor(private genericService: GenericService) {
+    this.nivel = new NivelDTO("","",0);
+   }
 
   ngOnInit() {
+    this.getNiveles();
   }
 
+  getNiveles(){
+    this.genericService.getAll(this.componentUrl, (niveles) => {
+      this.niveles = niveles;
+    });
+  }
+  crearNivel(){
+    this.genericService.crear(this.componentUrl, this.nivel, () => {
+      this.getNiveles();
+    });
+    
+  }
 
-  
+  borrarNivel(id: string){
+    this.genericService.borrar(this.componentUrl, id, () => {
+      this.getNiveles();
+    });
+  }
+
+  traerNivel(id: string){
+    this.genericService.getById(this.componentUrl, id, (nivel) => {
+      this.nivel = nivel;
+      this.editMode = true;
+      console.log(this.nivel);
+    });
+  }
+
+  actualizarNivel(id: string, nivel: Partial<NivelDTO>){
+    this.genericService.actualizar(this.componentUrl, id, nivel, () => {
+      this.nivel = new NivelDTO("","",0);
+      this.editMode = false;
+      this.getNiveles();
+    })
+  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DocumentoComponent } from './admin/components/documento/documento.component';
 import { InstitucionComponent } from './admin/components/institucion/institucion.component';
 import { HomeComponent } from './admin/components/home/home.component';
+import { NivelComponent } from './admin/components/nivel/nivel.component';
 
 
 const routes: Routes = [
+  {path: 'admin', component: HomeComponent},
   {path: 'admin/documento', component: DocumentoComponent},
   {path: 'admin/institucion', component: InstitucionComponent},
-  {path: 'admin', component: HomeComponent}
+  {path: 'admin/nivel', component: NivelComponent}
 ];
 
 @NgModule({
