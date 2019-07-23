5475274e1a8960025ad530282632a7d635258707
Phew <marcos_eduardo16@hotmail.com>
2019-07-23 19:25:51 -0400

    AAA

--- src/app/components/solicitud/solicitud.component.ts
+++ src/app/components/solicitud/solicitud.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
+  pagename: string = "Hacer una solicitud"
+
+
   constructor() { }
 
   ngOnInit() {
